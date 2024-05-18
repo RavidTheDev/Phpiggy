fdef8bfa151f4061f26c9cc9be338253ef2327c0
Ravid Lahamani <ravid3112@gmail.com>
2024-05-18 20:04:33 +0300

    update MacBook

--- src/Framework/router.php
+++ src/Framework/router.php
@@ -40,7 +40,12 @@ class Router
       ) {
         continue;
       }
-      echo 'route found';
+
+      [$class, $function] = $route['controller'];
+
+      $controllerInstance = new $class;
+
+      $controllerInstance->{$function}();
     }
   }
 }
