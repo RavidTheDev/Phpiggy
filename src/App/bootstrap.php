10f381fa3ce2416037d6b2dec737fe4fde20a551
Ravid Lahamani <ravid3112@gmail.com>
2024-05-23 07:53:28 +0300

    HP UPDATE

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require __DIR__ ."/../../vendor/autoload.php";
+require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
 use App\Controllers\HomeController;
@@ -10,7 +10,7 @@ use App\Controllers\HomeController;
 
 $app = new App();
 
-$app->get('/',[HomeController::class, 'home']);
+$app->get('/', [HomeController::class, 'home']);
 
 
 return $app;
